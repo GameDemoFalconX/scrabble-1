a00b6db9a82bb25813d82f2776abcfb4da56dbb9
Romain FONCIER <rfoncier@d-sight.com>
2012-11-19 10:07:35 +0100

    Update README file about message structure.
    Start work on playerRam and create XML file. Import JDOM library.

--- README.markdown
+++ README.markdown
@@ -6,4 +6,15 @@
 Scrabble is a word game in which two to four players score points by forming words from individual lettered tiles on a gameboard marked with a 15-by-15 grid. The words are formed across and down in crossword fashion and must appear in a standard dictionary.
 
 ### Details of implementation :
-Add more details here later ...
+*Message structure*
+
+	|----------| |----------------|
+	   header           body
+	           "#"
+	             |---------| |------------|
+	                token         args
+	                       "@"
+	                        |---------| |---------| | ...
+	                           args1       args2
+	                                  "_"
+
--- Scrabble Server/src/server/model/PlayerRAM.java
+++ Scrabble Server/src/server/model/PlayerRAM.java
@@ -2,25 +2,50 @@ package server.model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+import server.model.Player;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Import about XML generation, reader and writer
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 /**
  *
- * @author Bernard <bernard.debecker@gmail.com>
+ * @author Bernard <bernard.debecker@gmail.com>, Romain <ro.foncier@gmail.com>
  */
 public class PlayerRAM {
-    
-    private static Map<Integer, String> playerRAM;
-    
-    public PlayerRAM() {
-        //playerRAM = new HashMap<>();
-    }
-    
-    public PlayerRAM(boolean file) {
-        this();
-        if (file) {
-//            TODO get player from xml file (Romain)
-        }
-    }
+		
+		private  Map<String, Player> players = new HashMap<String, Player>();
+
+		public PlayerRAM() {
+				File playerFile = new File("players.xml");
+				if (playerFile.exists()) {
+						
+				} else {
+						try {
+								Element players = new Element("players");
+								Document doc = new Document(players);
+								doc.setRootElement(players);
+ 
+								XMLOutputter xmlOutput = new XMLOutputter();
+ 
+								xmlOutput.setFormat(Format.getPrettyFormat());
+								xmlOutput.output(doc, new FileWriter("players.xml"));
+ 
+								System.out.println("File Saved!");
+						} catch (IOException e) {
+								System.out.println(e.getMessage());
+						}
+				}
+		}
     
     public boolean playerNameExists(String name) {
         if (!playerRAM.isEmpty()) {
