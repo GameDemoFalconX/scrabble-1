e83a324e9dd7ba7c3516fb7d595e85b54295a736
Bernard <bernard.debecker@gmail.com>
2012-12-05 16:05:11 +0100

    Add Colors class and update Grid

--- Scrabble Client/src/client/model/Grid.java
+++ Scrabble Client/src/client/model/Grid.java
@@ -1,22 +1,13 @@
 package client.model;
 
+import common.Colors;
 
 /**
  *
  * @author Bernard <bernard.debecker@gmail.com>, Romain <ro.foncier@gmail.com>
  */
 public class Grid {
-		
-		public static final String ANSI_NORMAL = "\u001b[0m";
-  public static final String ANSI_WHITEONBLACK = "\u001b[37;40m";
-		public static final String ANSI_WHITEONWHITE = "\u001b[30;47m";
-		public static final String ANSI_WHITEONRED = "\u001b[37;41m";
-		public static final String ANSI_WHITEONMAGENTA = "\u001b[37;45m";
-		public static final String ANSI_WHITEONBLUE = "\u001b[37;44m";
-		public static final String ANSI_WHITEONCYAN = "\u001b[37;46m";
-		public static final String ANSI_WHITEONYELLOW = "\u001b[37;43m";
-		public static final String ANSI_BLACKONWHITE = "\u001b[30;47m";
-		
+	
 		private Tile[][] grid = new Tile[15][15];
 		private ScoringGrid scoringGrid = new ScoringGrid();
 		
@@ -34,38 +25,38 @@ public class Grid {
 		
 		@Override
 		public String toString() {
-				String prtGrid = ANSI_BLACKONWHITE + "      1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   " + ANSI_NORMAL;
-				prtGrid +=  ANSI_BLACKONWHITE +  "    ___________________________________________________________________________ \n" + ANSI_NORMAL;
+				String prtGrid = Colors.ANSI_BLACKONWHITE + "      1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   " + Colors.ANSI_NORMAL;
+				prtGrid +=  Colors.ANSI_BLACKONWHITE +  "    ___________________________________________________________________________ \n" + Colors.ANSI_NORMAL;
 				for (int x = 0; x <= 14 ; x++) {
 						if (x < 9) {
-								prtGrid += ANSI_BLACKONWHITE +  "0"+ (x+1) + " |" + ANSI_NORMAL;
+								prtGrid += Colors.ANSI_BLACKONWHITE +  "0"+ (x+1) + " |" + Colors.ANSI_NORMAL;
 						} else {
-								prtGrid += ANSI_BLACKONWHITE +  (x+1) + " |" + ANSI_NORMAL;
+								prtGrid += Colors.ANSI_BLACKONWHITE +  (x+1) + " |" + Colors.ANSI_NORMAL;
 						}
 						for (int y = 0; y <= 14; y++) {
 								Tile tile = grid[y][x];
 								if (tile != null) {
-										prtGrid += ANSI_WHITEONBLACK + grid[y][x] + ANSI_NORMAL;
+										prtGrid += Colors.ANSI_WHITEONBLACK + grid[y][x] + Colors.ANSI_NORMAL;
 								} else { 
 										switch (scoringGrid.getBonus(x,y)) {
 											 case	ScoringGrid.TRIPLE_WORD : 
-														prtGrid += ANSI_WHITEONRED + "[T W]" + ANSI_WHITEONWHITE;
+														prtGrid += Colors.ANSI_WHITEONRED + "[T W]" + Colors.ANSI_WHITEONWHITE;
 														break;
 												case	ScoringGrid.DOUBLE_WORD : 
-														prtGrid += ANSI_WHITEONMAGENTA + "[D W]" + ANSI_NORMAL;
+														prtGrid += Colors.ANSI_WHITEONMAGENTA + "[D W]" + Colors.ANSI_NORMAL;
 														break;
 												case	ScoringGrid.TRIPLE_LETTER : 
-														prtGrid += ANSI_WHITEONBLUE + "[T L]" + ANSI_NORMAL;
+														prtGrid += Colors.ANSI_WHITEONBLUE + "[T L]" + Colors.ANSI_NORMAL;
 														break;
 												case	ScoringGrid.DOUBLE_LETTER : 
-														prtGrid += ANSI_WHITEONCYAN + "[D L]" + ANSI_NORMAL;
+														prtGrid += Colors.ANSI_WHITEONCYAN + "[D L]" + Colors.ANSI_NORMAL;
 														break;
 												default :
-														prtGrid += ANSI_WHITEONWHITE + "     " + ANSI_NORMAL;
+														prtGrid += Colors.ANSI_WHITEONWHITE + "     " + Colors.ANSI_NORMAL;
 									 }					
 								}	
 						}		
-						prtGrid +=  ANSI_NORMAL + "" + ANSI_WHITEONWHITE + "\n" + ANSI_NORMAL;
+						prtGrid +=  Colors.ANSI_NORMAL + "" + Colors.ANSI_WHITEONWHITE + "\n" + Colors.ANSI_NORMAL;
 				}
 				return prtGrid;
 		}
