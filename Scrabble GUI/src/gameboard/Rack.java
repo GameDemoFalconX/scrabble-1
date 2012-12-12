cce1a455d36d5d516661eb34a3d6825cdb7534be
Arnaud MOREL <a.morel@hotmail.com>
2012-12-12 20:54:28 +0100

    Update Rack and Tile class

--- Scrabble GUI/src/gameboard/Rack.java
+++ Scrabble GUI/src/gameboard/Rack.java
@@ -2,13 +2,7 @@ package gameboard;
 
 import common.DTPicture;
 import common.TileTransferHandler;
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
@@ -17,73 +11,29 @@ import javax.swing.JPanel;
  */
 public class Rack extends JPanel {
 
-//  private Tile[] rack = new Tile[7];
-  DTPicture dticone;
+  private Tile[] rack = new Tile[7];
+  private int[] sizeTile = new int[2]; //Size image of the tile [width, height]
 
   public Rack(int ratingOfGUI){
     
-//    char TileRackLetterTest[] = {'S','C','R','A','B','B','L','E'};
-//    int TileRackValueTest[] = {1,4,1,8,8,2,1};
-//    for (int i = 0; i < rack.length; i++) {
-//      JPanel JPaneTileRack = new JPanel(new GridLayout(1, 1));
-//      Tile tile = new Tile(TileRackLetterTest[i], TileRackValueTest[i]);
-//      JPaneTileRack.add(tile);
-//      JPaneTileRack.setVisible(true);
-//      add(JPaneTileRack,i);
-//						rack[i] = tile;
-//				}
+    char TileRackLetterTest[] = {'S','C','R','A','B','B','L','E'};
+    int TileRackValueTest[] = {1,4,1,8,8,2,1};
     
-    ImageIcon icon = createImageIcon("images/Tile.png","Tile");
-    icon = new ImageIcon(getScaledImage(icon.getImage(), 36,getProportionnalHeight(icon, 37)));
-    dticone = new DTPicture(icon.getImage());
-    dticone.setTransferHandler(new TileTransferHandler());
-    JPanel JPaneTile1 = new JPanel(new GridLayout(1, 1));
-    JPaneTile1.add(dticone);
-    JPaneTile1.setVisible(true);
-    add(JPaneTile1,0);
-
+    for (int i = 0; i < rack.length; i++) {
+      JPanel JPaneTileRack = new JPanel(new GridLayout(1, 1));
+      Tile tile = new Tile(TileRackLetterTest[i], TileRackValueTest[i]);
+      DTPicture dticone = new DTPicture(tile.getImg());
+      sizeTile[0] = tile.getImg().getWidth(null);
+      sizeTile[1] = tile.getImg().getHeight(null);
+      dticone.setTransferHandler(new TileTransferHandler());
+      JPaneTileRack.add(dticone);
+      JPaneTileRack.setVisible(true);
+      add(JPaneTileRack,i);
+						rack[i] = tile;
+				}
+    
     setLayout(new java.awt.GridLayout(1, 7, 0, 0));
-    setBackground(Color.red);
-    setBounds( 200, ratingOfGUI*100 + 40, icon.getIconWidth()*8, icon.getIconHeight()+10);
+    setBounds( 200, ratingOfGUI*100 + 40, sizeTile[0]*8, sizeTile[1] +10);
     setVisible(true);
-  }
-
-  /** Returns an ImageIcon, or null if the path was invalid. */
-  final protected ImageIcon createImageIcon(String path, String description) {
-    java.net.URL imgURL = getClass().getResource(path);
-    if (imgURL != null) {
-      return new ImageIcon(imgURL, description);
-    } 
-    else {
-      System.err.println("Couldn't find file: " + path);
-      return null;
-    }
-  }
-
-  /**
-   * Return height from width proportionate to ImageIcon
-   * @param imgIco - Source ImageIcon
-   * @param icoWidth - new width of icon
-   * @return newHeight - the new proportionate height
-   */
-  private int getProportionnalHeight(ImageIcon imgIco, int icoWidth){
-    int newHeight = imgIco.getIconHeight() * icoWidth / imgIco.getIconWidth();
-    return newHeight;
-  }
-
-  /**
-   * Resizes an image using a Graphics2D object backed by a BufferedImage.
-   * @param srcImg - source image to scale
-   * @param w - desired width
-   * @param h - desired height
-   * @return - the new resized image
-   */
-  private Image getScaledImage(Image srcImg, int w, int h){
-    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-    Graphics2D g2 = resizedImg.createGraphics();
-    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-    g2.drawImage(srcImg, 0, 0, w, h, null);
-    g2.dispose();
-    return resizedImg;
   }
 }
--- Scrabble GUI/src/gameboard/Tile.java
+++ Scrabble GUI/src/gameboard/Tile.java
@@ -1,72 +1,100 @@
-//package gameboard;
-//
-//import common.DTPicture;
-//import common.TileTransferHandler;
-//import java.awt.Graphics2D;
-//import java.awt.Image;
-//import java.awt.RenderingHints;
-//import java.awt.image.BufferedImage;
-//import javax.swing.ImageIcon;
-//
-///**
-// * 
-// * @author Arnaud Morel <a.morel@hotmail.com>
-// */
-//public class Tile extends DTPicture {
-//    
-//  private char letter;
-//		private final int value;
-//
-//    public Tile(Image image){
-//      
-//    };
-//    
-//  public Tile(char letter, int value) {
-//				this.letter = letter;
-//				this.value = value;
-//    
-//    ImageIcon icon = createImageIcon("images/Tile.png","Tile");
-//    icon = new ImageIcon(getScaledImage(icon.getImage(), 36,getProportionnalHeight(icon, 37)));
-//    setImage(icon.getImage());
-//    setTransferHandler(new TileTransferHandler());
-//		}
-//  
-//  /** Returns an ImageIcon, or null if the path was invalid. */
-//  final protected ImageIcon createImageIcon(String path, String description) {
-//    java.net.URL imgURL = getClass().getResource(path);
-//    if (imgURL != null) {
-//      return new ImageIcon(imgURL, description);
-//    } 
-//    else {
-//      System.err.println("Couldn't find file: " + path);
-//      return null;
-//    }
-//  }
-//
-//  /**
-//   * Return height from width proportionate to ImageIcon
-//   * @param imgIco - Source ImageIcon
-//   * @param icoWidth - new width of icon
-//   * @return newHeight - the new proportionate height
-//   */
-//  private int getProportionnalHeight(ImageIcon imgIco, int icoWidth){
-//    int newHeight = imgIco.getIconHeight() * icoWidth / imgIco.getIconWidth();
-//    return newHeight;
-//  }
-//
-//  /**
-//   * Resizes an image using a Graphics2D object backed by a BufferedImage.
-//   * @param srcImg - source image to scale
-//   * @param w - desired width
-//   * @param h - desired height
-//   * @return - the new resized image
-//   */
-//  private Image getScaledImage(Image srcImg, int w, int h){
-//    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-//    Graphics2D g2 = resizedImg.createGraphics();
-//    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-//    g2.drawImage(srcImg, 0, 0, w, h, null);
-//    g2.dispose();
-//    return resizedImg;
-//  }
-//}
+package gameboard;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+
+/**
+ * 
+ * @author Arnaud Morel <a.morel@hotmail.com>
+ */
+
+ public final class Tile {
+		
+		private char letter;
+		private final int value;
+  private Image img;
+
+		public Tile(char letter, int value) {
+				this.letter = letter;
+				this.value = value;
+
+    ImageIcon icon = createImageIcon("images/Tile.png","Tile");
+    icon = new ImageIcon(getScaledImage(icon.getImage(), 36,getProportionnalHeight(icon, 37)));
+    setImg(icon.getImage());
+    
+		}
+
+		public void setImg(Image im) {
+    img = im;
+		}
+  
+		public Image getImg() {
+			return img;
+		}
+  
+		public char getLetter() {
+			return letter;
+		}
+
+		public int getValue() {
+				return value;
+		}
+		
+		public void setLetter(char letter) {
+				this.letter = letter;
+		}
+
+		/**
+		* Used only for debugging purpose
+		*/
+		@Override
+		public String toString() {
+				if (value < 10) {
+						return "[" + letter + " " + value + "]";
+				} else {
+						return "[" + letter + "" + value+ "]";
+				}
+		}   
+  
+  /** Returns an ImageIcon, or null if the path was invalid. */
+  final protected ImageIcon createImageIcon(String path, String description) {
+    java.net.URL imgURL = getClass().getResource(path);
+    if (imgURL != null) {
+      return new ImageIcon(imgURL, description);
+    } 
+    else {
+      System.err.println("Couldn't find file: " + path);
+      return null;
+    }
+  }
+
+  /**
+   * Return height from width proportionate to ImageIcon
+   * @param imgIco - Source ImageIcon
+   * @param icoWidth - new width of icon
+   * @return newHeight - the new proportionate height
+   */
+  private int getProportionnalHeight(ImageIcon imgIco, int icoWidth){
+    int newHeight = imgIco.getIconHeight() * icoWidth / imgIco.getIconWidth();
+    return newHeight;
+  }
+
+  /**
+   * Resizes an image using a Graphics2D object backed by a BufferedImage.
+   * @param srcImg - source image to scale
+   * @param w - desired width
+   * @param h - desired height
+   * @return - the new resized image
+   */
+  private Image getScaledImage(Image srcImg, int w, int h){
+    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+    Graphics2D g2 = resizedImg.createGraphics();
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g2.drawImage(srcImg, 0, 0, w, h, null);
+    g2.dispose();
+    return resizedImg;
+  }
+}
