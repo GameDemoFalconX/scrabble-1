d84a936dd290fa08971da4a1974c776dac77b6dd
Romain FONCIER <rphonika@gmail.com>
2012-12-17 15:03:16 +0100

    Start work on switch tiles within rack

--- Scrabble GUI/src/gameboard/Shade.java
+++ Scrabble GUI/src/gameboard/Shade.java
@@ -12,43 +12,41 @@ import javax.swing.JPanel;
  */
 public class Shade extends JPanel {
 
-  private ImageIcon icon;
-  private JPanel[] shadeList = new JPanel[7];
+		private ImageIcon icon;
+		private JPanel[] shadeList = new JPanel[7];
 
-  public Shade(){
-    
-    for (int i = 0; i < 7; i++) {
-      shadeList[i] = initSquareRack();
-      setOpaque(false);
-      add(shadeList[i],i);
+		public Shade(){
+				for (int i = 0; i < 7; i++) {
+						shadeList[i] = initSquareRack();
+						setOpaque(false);
+						add(shadeList[i],i);
 				}
-//    setBorder(BorderFactory.createLineBorder(Color.black));
-    setLayout(new java.awt.GridLayout(1, 7, 0, 0));
-    setBounds( 192, 723, 314, 37);
-    setVisible(true);
-  }
+				//setBorder(BorderFactory.createLineBorder(Color.black));
+				setLayout(new java.awt.GridLayout(1, 7, 0, 0));
+				setBounds( 192, 723, 314, 37);
+				setVisible(true);
+		}
+		
+		private JPanel initSquareRack() {
+				JPanel JPaneShadeTile = new JPanel(new GridLayout(1, 1));
+				JPaneShadeTile.add(new JLabel(getImageShade()));
+				JPaneShadeTile.setOpaque(false);
+				JPaneShadeTile.setVisible(true);
+				return JPaneShadeTile;
+		}
   
-  private JPanel initSquareRack() {
-      JPanel JPaneShadeTile = new JPanel(new GridLayout(1, 1));
-      JPaneShadeTile.add(new JLabel(getImageShade()));
-      JPaneShadeTile.setOpaque(false);
-      JPaneShadeTile.setVisible(true);
-      return JPaneShadeTile;
-  }
+		private ImageIcon getImageShade(){
+				icon = ImageTools.createImageIcon("images/Shade.png","Shade");
+				icon = new ImageIcon(ImageTools.getScaledImage(icon.getImage(), 36, ImageTools.getProportionnalHeight(icon, 37)));
+				return icon;
+		}
   
-  private ImageIcon getImageShade(){
-    icon = ImageTools.createImageIcon("images/Shade.png","Shade");
-    icon = new ImageIcon(ImageTools.getScaledImage(icon.getImage(), 36,
-                         ImageTools.getProportionnalHeight(icon, 37)));
-    return icon;
-  }
-  
-  /**
-   * Define either shade must be visible.
-   * @param position
-   * @param bol
-   */
-  public void setVisibleShade(int position, boolean bol){
-    shadeList[position-1].setVisible(bol);
-  }
+		/**
+		  * Define either shade must be visible.
+			* @param position
+			* @param bol
+		   */
+		public void setVisibleShade(int position, boolean bol){
+				shadeList[position-1].setVisible(bol);
+		}
 }
