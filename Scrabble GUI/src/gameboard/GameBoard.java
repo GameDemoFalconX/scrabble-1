b50d2e8ec5a44f06404107d2b48c10c0f0d847ba
Romain FONCIER <rfoncier@d-sight.com>
2012-12-18 19:02:46 +0100

    Start refactoring of Scrabble class (previously mainFrame class) and refactor the GameBoard class.

--- Scrabble GUI/src/gameboard/GameBoard.java
+++ Scrabble GUI/src/gameboard/GameBoard.java
@@ -1,29 +1,50 @@
 package gameboard;
 
-import common.ImageTools;
+import java.awt.Image;
+import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
- * 
- * @author Arnaud Morel <a.morel@hotmail.com>
- */
-public class GameBoard extends JPanel{
-  
-  private ImageIcon icon;
-  
-  public GameBoard(){
-    add(new JLabel(getImageGameBoard()));
-    setVisible(true);
-    setLayout(new java.awt.GridLayout(1, 1, 1, 1)); //Allow to get rid of the gap between JPanel and JLabel
-    setBounds( 0, 0, icon.getIconWidth(), icon.getIconHeight());
-  }
-  
-  public ImageIcon getImageGameBoard(){
-    icon = ImageTools.createImageIcon("images/Grid_72ppp.jpg","Game Board");
-    icon = new ImageIcon(ImageTools.getScaledImage(icon.getImage(), 700,
-                         ImageTools.getProportionnalHeight(icon, 700)));
-    return icon;
-  }
+	* 
+	* @author Arnaud Morel <a.morel@hotmail.com>, R. FONCIER <ro.foncier@gmail.com>
+	*/
+public class GameBoard extends JPanel {
+		
+		private ImageIcon icon;
+		private static final int GB_HEIGHT = 700;
+		private static final int GB_WIDTH = 700;
+		
+		public GameBoard(){
+				setImageGameBoard();
+				add(new JLabel(this.icon));
+				setVisible(true);
+				setLayout(new java.awt.GridLayout(1, 1, 1, 1)); //Allow to get rid of the gap between JPanel and JLabel
+				setBounds( 0, 0, icon.getIconWidth(), icon.getIconHeight());
+		}
+		
+		/**
+			* Set the image of the game board and resize it
+			* @see ImageIcon : An implementation of the Icon interface that paints Icons from Images
+			* @see Image
+			*/
+		private void setImageGameBoard(){
+				ImageIcon newIcon = createImageIcon("media/Scrabble_gameboard.png","Scrabble game board");
+				// SCALE_SMOOTH : Choose an image-scaling algorithm that gives higher priority to image smoothness than scaling speed.
+				Image iconScaled = newIcon.getImage().getScaledInstance(GB_WIDTH, GB_HEIGHT,  Image.SCALE_SMOOTH);
+				this.icon = new ImageIcon(iconScaled);
+		}
+		
+		/** Returns an ImageIcon, or null if the path was invalid. */
+		protected ImageIcon createImageIcon(String path, String description) {
+				ImageIcon newIcon = null;
+				URL imgURL = getClass().getResource(path);
+				if (imgURL != null) {
+						newIcon =  new ImageIcon(imgURL, description);
+				} else {
+						System.err.println("Couldn't find file : " + path);
+				}
+				return newIcon;
+		}		
 }
--- Scrabble GUI/src/gameboard/Scrabble.java
+++ Scrabble GUI/src/gameboard/Scrabble.java
@@ -1,75 +1,72 @@
 package gameboard;
 
-import common.LocateOfTile;
 import common.GlassPane;
-import java.awt.BorderLayout;
+import common.LocateOfTile;
 import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
- *
- * @author Arnaud Morel <a.morel@hotmail.com>, Bernard <bernard.debecker@gmail.com>
- */
-class MainFrame {
+	* Main class for Scrabble game
+	* @author Arnaud Morel <a.morel@hotmail.com>, Bernard <bernard.debecker@gmail.com>, R. FONCIER <ro.foncier@gmail.com>
+	*/
+public class Scrabble {
     
-  private JFrame frame;
-  private Container contentPane;
-  private GameBoard gameBoard;
-  private Rack rack;
-  private GameGrid gameGrid;
-  private Menu menu;
-  private JPanel rackBackground;
-  private Shade shadeTile;
+		private JFrame frame;
+		private Container contentPane;
+		private GameBoard gameBoard;
+		private Rack rack;
+		private GameGrid gameGrid;
+		private Menu menu;
+		private JPanel rackBackground;
+		private Shade shadeTile;
 
-  public MainFrame()  {
-    frame = new JFrame("Scrabble");
-    gameBoard = new GameBoard();
-    rack = new Rack();
+		public Scrabble()  {
+				frame = new JFrame("Scrabble");
+				gameBoard = new GameBoard();
+				rack = new Rack();
 				gameGrid = new GameGrid();
 				menu = new Menu();
-    shadeTile = new Shade();
-    new LocateOfTile(shadeTile);
-//    ShadeTile.setVisibleShade(1, false);
-    rackBackground = new RackBackground();
-    initContainer();
-    initFrame();
-  }
+				shadeTile = new Shade();
+				new LocateOfTile(shadeTile);
+				// ShadeTile.setVisibleShade(1, false);
+				rackBackground = new RackBackground();
+				initContainer();
+				initFrame();
+		}
 
-  private void initContainer() {
-    contentPane =  frame.getContentPane() ;
-    contentPane.setLayout(null);
-    contentPane.add(gameBoard, 0);
-    contentPane.add(rackBackground, 0);
-    contentPane.add(shadeTile, 0);
-    contentPane.add(rack, 0);
-    contentPane.add(gameGrid, 0);
-    contentPane.setVisible(true);
-  }
-  
-  private void initFrame() {
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		private void initContainer() {
+				contentPane =  frame.getContentPane() ;
+				contentPane.setLayout(null);
+				contentPane.add(gameBoard, 0);
+				contentPane.add(rackBackground, 0);
+				contentPane.add(shadeTile, 0);
+				contentPane.add(rack, 0);
+				contentPane.add(gameGrid, 0);
+				contentPane.setVisible(true);
+		}
+		
+		private void initFrame() {
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setJMenuBar(menu.getMenu());
-    frame.setSize(gameBoard.getWidth() + gameBoard.getInsets().left
-                + gameBoard.getInsets().right+15, 850);
-    frame.setContentPane(contentPane);
-    frame.setGlassPane(GlassPane.getInstance());
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+				frame.setSize(gameBoard.getWidth() + gameBoard.getInsets().left+gameBoard.getInsets().right+15, 850);
+				frame.setContentPane(contentPane);
+				frame.setGlassPane(GlassPane.getInstance());
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
 				SideMenu.setVisible(true);
 		}
-	
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		/**
+		  * @param args the command line arguments
+		  */
+		public static void main(String[] args) {
+				javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				
 						@Override
 						public void run() {
-										MainFrame mainFrame = new MainFrame();
+								Scrabble scrabble = new Scrabble();
 						}
-    });
-  }
-    
+				});
+		}
 }
