d92fc6348e4a87444c265094c1d5f88852b20c02
Romain FONCIER <rfoncier@d-sight.com>
2012-12-20 19:11:45 +0100

    Clean DTPicture

--- Scrabble GUI/src/common/DTPicture.java
+++ Scrabble GUI/src/common/DTPicture.java
@@ -22,7 +22,6 @@ import java.awt.Color;
 	*/
 public class DTPicture extends Picture implements MouseMotionListener {
 
-		private MouseEvent firstMouseEvent = null;
 		private GlassPane glass;
 		private BufferedImage imageGlass;
 		private boolean isLocked = false;
@@ -56,14 +55,8 @@ public class DTPicture extends Picture implements MouseMotionListener {
 		/*** Methods from interface MouseListener ***/
 		@Override
 		public void mousePressed(MouseEvent e) {
-				Component comp = e.getComponent(); // Obviously that concerns a Tile
+				Component comp = e.getComponent(); // Obviously that concerns (a Tile within) a DTPicture instance
 				Point location = (Point)e.getPoint().clone();
-				
-				if (this.image == null ) {
-						System.out.println("No Tile!");
-						LocateOfTile.setDndEnable(false);
-				}
-				LocateOfTile.locateTile("From");
 				
 				// Convert a point from a component's coordinate system to screen coordinates.
 				SwingUtilities.convertPointToScreen(location, comp);
@@ -78,11 +71,6 @@ public class DTPicture extends Picture implements MouseMotionListener {
 				glass.setImage(imageGlass);
 				glass.setVisible(true);
 
-				//Don't bother to drag if there is no image.
-				if (this.image == null) {
-						return;
-				}
-				firstMouseEvent = e;
 				// Once an InputEvent is consumed, the source component will not process the event itself. 
 				// However, the event will still be dispatched to all registered listeners.
 				e.consume();
@@ -90,11 +78,7 @@ public class DTPicture extends Picture implements MouseMotionListener {
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-				firstMouseEvent = null;
 				setVisible(true);
-				if (LocateOfTile.getLockDropOnTile()) {
-						LocateOfTile.switchTiles();
-				}
 						
 				JComponent jComp = (JComponent)e.getSource(); // It concerns an instance of DTPicture
 				TransferHandler tHandler = jComp.getTransferHandler(); // Get the instance of TransferHandler for this component
@@ -105,8 +89,6 @@ public class DTPicture extends Picture implements MouseMotionListener {
 				
 				// Convert a point from a component's coordinate system to screen coordinates.
 				SwingUtilities.convertPointToScreen(location, comp);
-				int CooX = location.x+1;
-				int CooY = location.y+1;
 				// Convert a point from a screen coordinates to a component's coordinate system.
 				SwingUtilities.convertPointFromScreen(location, glass);
 
@@ -114,15 +96,6 @@ public class DTPicture extends Picture implements MouseMotionListener {
 				glass.setLocation(location);
 				glass.setImage(null);
 				glass.setVisible(false);
-
-				// Movement mouse for the refresh's display.
-				/*
-				try {
-						Robot robot = new Robot();
-						robot.mouseMove(CooX, CooY);
-				} catch (AWTException ex) {
-						Logger.getLogger(DTPicture.class.getName()).log(Level.SEVERE, null, ex);
-				}*/
 		}
 		
 		/*** Methods from interface MouseMotionListener ***/
@@ -136,15 +109,6 @@ public class DTPicture extends Picture implements MouseMotionListener {
 				SwingUtilities.convertPointFromScreen(location, glass);
 				glass.setLocation(location);
 				glass.repaint();
-
-				// Don't bother to drag if the component displays no image.
-				if (this.image == null) {
-						return;
-				}
-				if (firstMouseEvent != null) {
-						// Consumes this event so that it will not be processed in the default manner by the source which originated it. (inhereted from InputEvent)
-						e.consume();
-				}
 		}
 
 		@Override
