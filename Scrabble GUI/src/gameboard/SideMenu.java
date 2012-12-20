f3e94dee273c636158cc81cf943a6f93e43b7f3d
Bernard Debecker <bernard.debecker@gmail.com>
2012-12-20 21:54:57 +0100

    Add email and password fields to side menu

--- Scrabble GUI/src/gameboard/SideMenu.java
+++ Scrabble GUI/src/gameboard/SideMenu.java
@@ -1,12 +1,11 @@
 package gameboard;
 
+import common.EmailValidator;
 import common.ImageTools;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
@@ -19,8 +18,12 @@ public class SideMenu {
 	 private static JPanel panel, dropDownMenu;
 	 private JButton playerButton;
 		private JPopupMenu popupMenu;
-		private JMenuItem logSign, logOff;
+		private JMenuItem logIn, signUp, logOff;
 		private String email;
+		private JTextField emailField;
+		private JPasswordField passwordField;
+		private char[] password;
+		private EmailValidator emailValidator;
 
 		public SideMenu()	{
 				panel = new JPanel();
@@ -31,6 +34,7 @@ public class SideMenu {
 		}
 	
 		private void initComponent() {
+				emailValidator = new EmailValidator();
 				initPopupMenu();
 				initPlayerButton();
 				panel.add(playerButton);
@@ -54,10 +58,50 @@ public class SideMenu {
 		
 		private void initPopupMenu() {
 				popupMenu = new JPopupMenu();
-				logSign = new JMenuItem(new AbstractAction("Log in/Sign up") {
+				emailField = new JTextField("Email");
+				emailField.addFocusListener(new FocusListener() {
+
+						@Override
+						public void focusGained(FocusEvent e) {
+								if (emailField.getText().equals("Email")) {
+            emailField.setText("");
+        }
+						}
+
+						@Override
+						public void focusLost(FocusEvent e) {
+								if (emailField.getText().equals("")) {
+            emailField.setText("Email");
+        }
+						}
+				});
+				passwordField = new JPasswordField("Password");
+				passwordField.addFocusListener(new FocusListener() {
+
+						@Override
+						public void focusGained(FocusEvent e) {
+								if (emailField.getText().equals("Password")) {
+            emailField.setText("");
+        }
+						}
+
+						@Override
+						public void focusLost(FocusEvent e) {
+								if (emailField.getText().equals("")) {
+            emailField.setText("Password");
+        }
+						}
+				});
+				logIn = new JMenuItem(new AbstractAction("Log in") {
 						@Override
 						public void actionPerformed(ActionEvent e) {
-								JOptionPane.showMessageDialog(null, "Log in/Sign up comes here");
+								logInSignUp();
+      }
+				});
+				signUp = new JMenuItem(new AbstractAction("Sign Up") {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+								logInSignUp();
       }
 				});
 				logOff = new JMenuItem(new AbstractAction("Log off") {
@@ -66,11 +110,33 @@ public class SideMenu {
 								JOptionPane.showMessageDialog(null, "Log off");
       }
 				});
-				popupMenu.add(logSign);
+				popupMenu.add(emailField);
+				popupMenu.add(passwordField);
+				popupMenu.add(logIn);
+				popupMenu.add(signUp);
 				popupMenu.add(logOff);
 		}
 		
+		private void focusGained(FocusEvent e) {
+        if (emailField.getText().equals("Email")) {
+            //jTextArea1.setSelectionStart(0);
+            //jTextArea1.setSelectionEnd(jTextArea1.getText().length());
+            emailField.selectAll();
+        }
+    }
 		
+		
+		private void logInSignUp() {
+				email = emailField.getText();
+				password = passwordField.getPassword();
+				if (EmailValidator.validate(email)) {
+						playerButton.setIcon(ImageTools.getGravatar(email));
+				} else {
+						JOptionPane.showInternalMessageDialog(null, email + "is not a valid "
+														+ "email address");
+				}
+		}
+			
 		public static void setVisible(boolean visible) {
 				panel.setVisible(visible);
 		}
