de4490e481a03e8f02388aaf84466b4932497430
Romain FONCIER <rfoncier@d-sight.com>
2012-12-31 15:38:51 +0100

    Update tileTH when the drop failed. Set visible the DTP source.

--- Scrabble GUI/src/common/TileTransferHandler.java
+++ Scrabble GUI/src/common/TileTransferHandler.java
@@ -1,5 +1,6 @@
 package common;
 
+import java.awt.Component;
 import java.awt.Image;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -19,39 +20,56 @@ public class TileTransferHandler extends TransferHandler {
 		private static final DataFlavor flavors[] = { DataFlavor.imageFlavor };  // In your case the DataFlavor is representing by an image.
 		private DTPicture DTElement;
 		private JPanel parentContainer;
-		private boolean shouldRemove; // Define if the source element must be remove whether the target component is able to drop the DTElement.
+		private static boolean removeParent;
 
 		/*** TransferHandler - Import Methods ***/
 		
 		@Override
 		public boolean canImport(TransferSupport support) {
+				// Returns whether or not the given data flavor is supported.
 				if (!support.isDataFlavorSupported(flavors[0])) {
 						return false;
 				}
+				
+				if (support.getComponent() instanceof DTPicture) {
+						System.out.println("Find DTPicture target");
+						return false;
+				}
+				
+				System.out.println("Target class : "+support.getComponent().getParent().getClass());
 				return true;
 		}
 		
 		@Override
 		public boolean importData(TransferSupport support) {
+				System.out.println("Start import");
 				if (!canImport(support)) {
+						System.out.println("Import failed");
 						return false;
 				}
 				
+				System.out.println("Import success");
 				// Fetch the Transferable and its data (in our case this is an image)
 				Transferable t = support.getTransferable();
 				Image data = null;
 				try {
 						data = (Image) t.getTransferData(DataFlavor.imageFlavor);
+						System.out.println("Start import data");
 				} catch (UnsupportedFlavorException e) {
 						System.out.println("Error with DataFlavor type");
 				} catch (IOException IOe) {
 						System.out.println("importData: I/O exception");
 				}
-
+				System.out.println("Data import success");
 				// Create a new DTPicture element from the image transferred and add it to the target container (panelGrid or panelRack)
 				DTPicture dtp = new DTPicture(data);
+				System.out.println("Start import 2");
+				System.out.println("Parent import : "+support.getComponent().getClass());
 				JPanel parent = (JPanel) support.getComponent();		
+				System.out.println("Parent import : "+parent.getClass());
 				parent.add(dtp);
+				TileTransferHandler.removeParent = true;
+				System.out.println("Start import 3");
 				// The validate method is used to cause a container to lay out its subcomponents again. It should be invoked 
 				// when this container's subcomponents are modified (added to or removed from the container, 
 				// or layout-related information changed) after the container has been displayed.
@@ -74,8 +92,12 @@ public class TileTransferHandler extends TransferHandler {
 				System.out.println("Create Transferable");
 				DTElement = (DTPicture) c;				
 				parentContainer = (JPanel) DTElement.getParent();
+				Component [] l = parentContainer.getComponents();
+				for (int i = 0; i < l.length; i++) {
+						System.out.println("Parent sons ("+i+") : "+l[i].getClass());
+				}
 				displaySituation(parentContainer);
-				shouldRemove = true;
+				TileTransferHandler.removeParent = false;
 				return new PictureTransferable(DTElement);
 		}
 
@@ -83,10 +105,22 @@ public class TileTransferHandler extends TransferHandler {
 		protected void exportDone(JComponent c, Transferable t, int action) {
 				// This method is invoked after the export is complete. When the action is a MOVE, 
 				// the data needs to be removed from the source after the transfer is complete
-				if (shouldRemove && (action == MOVE)) {
+				System.out.println("Parent class exportDone : "+parentContainer.getClass());
+				System.out.println("remove : "+removeParent);
+				if (TileTransferHandler.removeParent && (action == MOVE)) {
+						Component [] l = parentContainer.getComponents();
+						for (int i = 0; i < l.length; i++) {
+								System.out.println("exportDoneBefore - Parent sons ("+i+") : "+l[i].getClass());
+						}
 						parentContainer.remove(DTElement); // Remove this DTElement from this parent container.
+						parentContainer.validate();
 						parentContainer.repaint();
+						Component [] k = parentContainer.getComponents();
+						for (int i = 0; i < k.length; i++) {
+								System.out.println("exportDoneAfter - Parent sons ("+i+") : "+k[i].getClass());
+						}
 				}
+				c.setVisible(true);
 				System.out.println("End export done");
 		}
 		
