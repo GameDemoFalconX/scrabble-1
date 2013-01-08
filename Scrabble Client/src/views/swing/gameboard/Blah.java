eebdb061a9ae059899f13a9d620577c69e7fa3ec
Bernard Debecker <bernard.debecker@gmail.com>
2013-01-08 21:58:34 +0100

    Update the help text

--- Scrabble Client/src/views/swing/gameboard/Blah.java
+++ Scrabble Client/src/views/swing/gameboard/Blah.java
@@ -8,10 +8,14 @@ public class Blah {
 
 		public static final String HELP = "To place a word"
 										+ " simply drag and drop the letters you want on the gameboard then click"
-										+ " the \"Add word\" button.\n\nIf the word is correct, you will see your"
-										+ " score incrementing and new letters will appear on your rack.\n\nIf"
+										+ " the \"Validate\" button.\n\nIf the word is correct, you will see your"
+										+ " score incrementing and new letters will appear on your rack.\nIf"
 										+ " the word is incorrect, you will see your score decrementing and the"
-										+ " letters will go back to your rack.";
+										+ " letters will go back to your rack.\n\nThe \"Exchange\" button allows"
+										+ " you to change tiles you don't want.\nThe \"Shuffle\" button allows"
+										+ " you to shuffle you rack in case you're short on ideas.\nThe \"Settings\""
+										+ " button allows you to change the gameboard style and the background color."
+										+ " \n\nGood luck !";
 		
 		public static final String ABOUT = "Scrabble is a word game in which two to"
 										+ " four players score points by forming words from individual lettered"
