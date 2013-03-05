1851621abaaa624e9cbc7a71b5aeb0f0747c352a
Romain FONCIER <rphonika@gmail.com>
2013-03-05 16:44:19 +0100

    Add two new methods to send request to the DB (similar to mechanism of GET and POST requests).

--- scrabble_server/src/main/java/server/server/db/Connector.java
+++ scrabble_server/src/main/java/server/server/db/Connector.java
@@ -2,10 +2,10 @@ package server.server.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * @author Romain <ro.foncier@gmail.com>
@@ -47,15 +47,34 @@ public class Connector {
         return false;
     }
     
-    private String execQuery(String query) {
+    /**
+     * This method must be used only for SELECT queries.
+     * @param query
+     * @return JSON format of SQL query result.
+     */
+    private String execGetQuery(String query, String[] args) {
         String response = null;
-        Statement stmt = null;
+        PreparedStatement stmt = null;
         ResultSet res = null;
         if (setUpConnection()) {
             try {
-                stmt = conn.createStatement();
+                stmt = conn.prepareStatement(query);
+                if (args != null) {
+                    for (int i = 0; i < args.length; i++) {
+                        if (args[i] instanceof String) {
+                            stmt.setString(i+1, args[i]);
+                        } else {
+                            try { 
+                                int value = Integer.parseInt(args[i]);
+                                stmt.setInt(i+1, value);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Error during prepared statement : type not found");
+                            }
+                        }
+                    }
+                }
                 stmt.setQueryTimeout(Timeout);
-                res = stmt.executeQuery(query);
+                res = stmt.executeQuery();
                 response = formatInJSON(res);
             } catch (SQLException se) {
                 se.printStackTrace();
@@ -70,10 +89,60 @@ public class Connector {
         }
         return response;
     }
+    
+    /**
+     * This method must be invoked only for INSERT, UPDATE and DELETE actions !
+     * PreparedStatement for insure protection against SQL Injection ! ;-)
+     * @param query, args 
+     */
+    private void execPostQuery(String query, String[] args) {
+        PreparedStatement stmt = null;
+        int res;
+        if (setUpConnection()) {
+            try {
+                stmt = conn.prepareStatement(query);
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i] instanceof String) {
+                        stmt.setString(i+1, args[i]);
+                    } else {
+                        try { 
+                            int value = Integer.parseInt(args[i]);
+                            stmt.setInt(i+1, value);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Error during prepared statement : type not found");
+                        }
+                    }
+                }
+                stmt.setQueryTimeout(Timeout);
+                res = stmt.executeUpdate();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } finally {
+                try {
+                    stmt.close();
+                } catch (Exception ignore) {}
+            }
+            closeConnection();
+        }
+    }
 
     // Methods used to get value on the DB
-    protected String getAllUsers() {
-        return "{\"users\" : "+execQuery("SELECT * from scrabble_user")+"}";
+    public String getAllUsers() {
+        return "{\"users\" : "+execGetQuery("SELECT * from scrabble_user", null)+"}";
+    }
+    
+    public String getUserById(String user_id) {
+        return execGetQuery("SELECT * from scrabble_user WHERE user_id = ?", new String[]{user_id});
+    }
+    
+    public String getUserByEmail(String user_email) {
+        return execGetQuery("SELECT * from scrabble_user WHERE email = ?", new String[]{user_email});
+    }
+    
+    public String createPlayer(String pl_email, String pl_pwd) {
+        String response = null;
+        
+        return response;
     }
 
     // Methods used to format the results of DB requests in JSON
@@ -114,6 +183,11 @@ public class Connector {
     
     public static void main(String[] args) {
         Connector c = new Connector();
-        System.out.println(c.getAllUsers());
+        //String query = "INSERT INTO scrabble_user ('user_id', 'username', 'email', 'password', 'created') VALUES ('d1293462-a0c5-4f7d-a330-d051042bab9e', 'romain1', 'romain@example.com', 'passwd', '2013/03/05')";
+        //String[][] cargs = new String[5][5];
+        //c.execPostQuery(null, args);
+        //System.out.println(c.getAllUsers());
+        //System.out.println(c.getUserById("d1293462-a0c5-4f7d-a330-d051042bab9f"));
+        System.out.println(c.getUserByEmail("romain@example.comm"));
     }
 }
