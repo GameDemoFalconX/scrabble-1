4bb0dd51a2eb1f5af9a191e160ecf506209a3a7b
Romain FONCIER <rphonika@gmail.com>
2013-03-05 14:38:24 +0100

    Update DB Connector and add a universal JSON format method.

--- scrabble_server/src/main/java/server/server/db/Connector.java
+++ scrabble_server/src/main/java/server/server/db/Connector.java
@@ -3,6 +3,7 @@ package server.server.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -42,13 +43,11 @@ public class Connector {
         try {
             conn.close();
             return true;
-        } catch (Exception ignore) {
-        }
+        } catch (Exception ignore) {}
         return false;
     }
-
-    // Methods used to get value on the DB
-    protected String getAllUsers() {
+    
+    private String execQuery(String query) {
         String response = null;
         Statement stmt = null;
         ResultSet res = null;
@@ -56,7 +55,6 @@ public class Connector {
             try {
                 stmt = conn.createStatement();
                 stmt.setQueryTimeout(Timeout);
-                String query = "SELECT * from scrabble_user";
                 res = stmt.executeQuery(query);
                 response = formatInJSON(res);
             } catch (SQLException se) {
@@ -73,21 +71,49 @@ public class Connector {
         return response;
     }
 
+    // Methods used to get value on the DB
+    protected String getAllUsers() {
+        return "{\"users\" : "+execQuery("SELECT * from scrabble_user")+"}";
+    }
+
     // Methods used to format the results of DB requests in JSON
     private String formatInJSON(ResultSet res) {
+        String result = "[";
         try {
+            ResultSetMetaData struc = res.getMetaData();
             while (res.next()) {
-                String sResult = res.getString("username");
-                System.out.println(sResult);
+                result += "{";
+                for (int i = 1; i < struc.getColumnCount(); i++) {
+                    String type = struc.getColumnTypeName(i);
+                    String columnName = struc.getColumnName(i);
+                    if (!type.equals("null")) {
+                        //System.out.println("Name : "+columnName+" - Type : "+type);
+                        result += "\""+struc.getColumnName(i)+"\" : ";
+                        switch(type) {
+                            case "text":
+                                result += "\""+res.getString(columnName) +"\"";
+                                break;
+                            case "integer":
+                                result += res.getInt(columnName);
+                                break;
+                            case "none":
+                                result += "\""+res.getBlob(columnName) +"\"";
+                                break;
+                        }
+                        result += (i < struc.getColumnCount()-1) ? ", " : "";
+                    }
+                }
+                result += (res.next()) ? "}, " : "}";
             }
         } catch (SQLException se) {
             se.printStackTrace();
         }
-        return "";
+        result += "]";
+        return result;
     }
     
     public static void main(String[] args) {
         Connector c = new Connector();
-        c.getAllUsers();
+        System.out.println(c.getAllUsers());
     }
 }
